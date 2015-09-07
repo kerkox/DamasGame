fdd76b0b2037759d29dc34e2b6e25bbd1e481aa0
Kerkox <bryanpoolker@gmail.com>
2015-09-06 21:39:05 -0500

    Version 0.0.2 +Verificacion de movimiento valido, metodo no testeado

--- src/damas/Damas.java
+++ src/damas/Damas.java
@@ -12,18 +12,19 @@ public class Damas {
      */
     public static void main(String[] args) {
         
-        Juego game = new Juego(8,8);
-        
-        System.out.println("Mostrando el tablero antes de iniciar");
-        System.out.println(game.showTablero());
-        
-        Jugador j1 = new Jugador('N'); 
-        Jugador j2 = new Jugador('B');
-        game.inicio(j1, j2);
-        
-        System.out.println("Tablero inciado: ");
-        System.out.println(game.showTablero());
-         
+//        Juego game = new Juego(8,8);
+//        
+//        System.out.println("Mostrando el tablero antes de iniciar");
+//        System.out.println(game.showTablero());
+//        
+//        Jugador j1 = new Jugador('N'); 
+//        Jugador j2 = new Jugador('B');
+//        game.inicio(j1, j2);
+//        
+//        System.out.println("Tablero inciado: ");
+//        System.out.println(game.showTablero());
+        int a=2, b=2;
+        System.out.println("resto de: "+ b +" entre "+ a +" es: "+ ((b%a)-1));
     }
     
 }
